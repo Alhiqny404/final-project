23660a5acc8d310c74d3f4acc59de53711d8cb38
Alhiqny404 <muhammadalhiqny@gmail.com>
2021-08-07 10:41:27 +0700

    update file db

--- final_project (2).sql
+++ final_project (2).sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Waktu pembuatan: 06 Agu 2021 pada 11.15
+-- Waktu pembuatan: 07 Agu 2021 pada 10.40
 -- Versi server: 5.6.38
 -- Versi PHP: 7.4.3
 
@@ -32,15 +32,21 @@ CREATE TABLE `document` (
   `document_name` varchar(227) NOT NULL,
   `document_file` varchar(227) NOT NULL,
   `document_status` varchar(100) NOT NULL,
-  `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
+  `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `responsed_at` datetime NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
 --
 -- Dumping data untuk tabel `document`
 --
 
-INSERT INTO `document` (`document_id`, `document_name`, `document_file`, `document_status`, `created_at`) VALUES
-(8, 'izin bacok', 'izin_bacok_1628213012..xlsx', 'setuju', '2021-08-06 08:23:32');
+INSERT INTO `document` (`document_id`, `document_name`, `document_file`, `document_status`, `created_at`, `responsed_at`) VALUES
+(1, 'izin bacok', 'izin_bacok_1628262187_1628264519.xlsx', 'setuju', '2021-08-06 22:03:07', '2021-08-06 22:41:59'),
+(2, 'izin bolos', 'izin_bolos_1628262205_1628296920.xlsx', 'tolak', '2021-08-06 22:03:25', '2021-08-07 07:42:00'),
+(3, 'Coba aja', 'Coba_aja_1628262234.xlsx', 'tolak', '2021-08-06 22:03:54', '0000-00-00 00:00:00'),
+(4, 'dokumen baru', 'dokumen_baru_1628300067.xlsx', 'pending', '2021-08-07 08:34:27', '0000-00-00 00:00:00'),
+(5, 'izin mati', 'izin_mati_1628300095.xlsx', 'tolak', '2021-08-07 08:34:55', '0000-00-00 00:00:00'),
+(6, 'Dokumen Dua', 'Dokumen_Dua_1628306565_1628306648.xlsx', 'setuju', '2021-08-07 10:22:45', '2021-08-07 10:24:08');
 
 -- --------------------------------------------------------
 
@@ -88,7 +94,7 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT untuk tabel `document`
 --
 ALTER TABLE `document`
-  MODIFY `document_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `document_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT untuk tabel `user`
